59ca39437d837b3bf60f5894818c4defae275135
Kenny91 <odessa5d4@gmail.com>
2021-11-22 17:04:57 +0900

    Octree.cpp texture3d format update

--- Narco/src/Octree.cpp
+++ Narco/src/Octree.cpp
@@ -8,7 +8,7 @@ namespace NARCO
 		D3D11_TEXTURE3D_DESC octreeTexDesc{};
 		D3D11_UNORDERED_ACCESS_VIEW_DESC octreeTexUAVDesc{};
 		
-		octreeTexDesc.Format = DXGI_FORMAT_R32G32_UINT;
+		octreeTexDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 		octreeTexDesc.Width = ChannelSize; // 2^6 
 		octreeTexDesc.Height = ChannelSize;
 		octreeTexDesc.Depth = ChannelSize;
@@ -16,7 +16,7 @@ namespace NARCO
 		octreeTexDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS;
 		octreeTexDesc.MipLevels = 1;
 		
-		octreeTexUAVDesc.Format = DXGI_FORMAT_R32G32_UINT;
+		octreeTexUAVDesc.Format = octreeTexDesc.Format;
 		octreeTexUAVDesc.Texture3D.WSize = ChannelSize;
 		octreeTexUAVDesc.ViewDimension = D3D11_UAV_DIMENSION_TEXTURE3D;
 		HRESULT result = mDevice->CreateTexture3D(&octreeTexDesc, nullptr, mTexture.GetAddressOf());
