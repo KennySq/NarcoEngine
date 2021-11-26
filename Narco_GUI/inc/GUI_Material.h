da515b0b8ddef09f53792e7965e61bfe1b031807
Kenny91 <odessa5d4@gmail.com>
2021-11-26 13:22:51 +0900

    GUI_Material.h update

--- Narco_GUI/inc/GUI_Material.h
+++ Narco_GUI/inc/GUI_Material.h
@@ -1,55 +1,55 @@
 #pragma once
-#include<Common.h>
-#include<Material.h>
-
 #include"IGUI.h"
 #include"ImGui_Texture.h"
+
 #include"GUI_FileSlot.h"
+
+#include<Asset.h>
+#include<Material.h>
+#include<Renderer.h>
 
 namespace NARCO
 {
-
-	/*enum eMaterialInputType
-	{
-		INPUT_SCALAR,
-		INPUT_COLOR2,
-		INPUT_COLOR3,
-		INPUT_COLOR4,
-		INPUT_MATRIX4X4,
-		INPUT_TEXTURE2D,
-		INPUT_TEXTURE3D,
-	};*/
-
-	struct MaterialInput
-	{
-		MaterialInput() {}
-		MaterialInput(void* data, uint size)
-			: Data(data), Size(size) {}
-
-		void* Data;
-		uint Size;
-	};
-
-
 	class GUI_Material : public IGUI
 	{
-		PUBLIC_API NARCO_API GUI_Material(Material* material, ID3D11Device* device, uint materialIndex);
-		PUBLIC_API NARCO_API virtual ~GUI_Material();
-		PUBLIC_API virtual NARCO_API void Start() override;
-		PUBLIC_API virtual NARCO_API void Update() override;
-		PUBLIC_API virtual NARCO_API void Draw() override;
-		PUBLIC_API virtual NARCO_API void End() override;
-		
-		PRIVATE_API NARCO_API void loadTexture(GUI_FileSlot* slot, MaterialProperty* mp);
+	public:
+		NARCO_API GUI_Material(ID3D11Device* device);
 
-		PRIVATE_PROPERTY ID3D11Device* mDevice;
-		PRIVATE_PROPERTY Material* mMaterial = nullptr;
-		PRIVATE_PROPERTY std::map<const char*, MaterialInput> mConstantVariables;
-		PRIVATE_PROPERTY uint mMaterialIndex;
+		NARCO_API virtual void Start() override;
+		NARCO_API virtual void Update() override;
+		NARCO_API virtual void Draw() override;
+		NARCO_API virtual void End() override;
 
-		PRIVATE_PROPERTY std::vector<GUI_FileSlot> mGUIFileSlots;
 
-		bool* bTextureLoadOpen;
+		NARCO_API void SetGameObject(GameObject* gameObject)
+		{
+			mGameObject = gameObject;
+			Renderer* renderer = gameObject->GetComponent<Renderer>();
+			if (renderer == nullptr)
+			{
+				mGameObject = nullptr;
+				return;
+			}
+
+			Material** materials = renderer->GetMaterials();
+			uint materialCount = renderer->GetMaterialCount();
+
+			for (uint i = 0; i < materialCount; i++)
+			{
+				mMaterials.push_back(materials[i]);
+			}
+
+			onChangeGameObject();
+		}
+
+	private:
+		NARCO_API void onChangeGameObject();
+
+		ID3D11Device* mDevice;
+		std::vector<Material*> mMaterials;
+		std::vector<std::vector<GUI_FileSlot*>> mTextureSlots;
+
+		GameObject* mGameObject;
 
 	};
 }
