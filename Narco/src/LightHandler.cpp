682d2833131b9674ac0ab4119b8695bc1c38becd
Kenny91 <odessa5d4@gmail.com>
2021-12-02 14:55:46 +0900

    LightHandler.cpp update

--- Narco/src/LightHandler.cpp
+++ Narco/src/LightHandler.cpp
@@ -2,6 +2,7 @@
 
 namespace NARCO
 {
+	LightHandler* LightHandler::mInstance = nullptr;
 	LightHandler::LightHandler()
 	{
 		HRESULT result = mDevice->CreateClassLinkage(mLinkage.GetAddressOf());
@@ -16,20 +17,75 @@ namespace NARCO
 	LightHandler::~LightHandler()
 	{
 	}
-	XMFLOAT3 PointLight::Diffuse(XMFLOAT3 normal)
+	bool LightHandler::GenerateDirectionals(std::vector<DirectionalLight>& lights)
 	{
-		
-		return XMFLOAT3();
+		D3D11_BUFFER_DESC directionalBufferDesc{};
+
+		directionalBufferDesc.ByteWidth = sizeof(DirectionalLight) * MAX_DIRECTIONAL_LIGHT;
+		directionalBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+		directionalBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		directionalBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+
+		D3D11_SUBRESOURCE_DATA subData{};
+
+		subData.pSysMem = lights.data();
+
+		HRESULT result = mDevice->CreateBuffer(&directionalBufferDesc, &subData, mDirectionalLightBuffer.GetAddressOf());
+		if (result != S_OK)
+		{
+			Debug::Log("failed to generate real-time light buffer.");
+			return false;
+		}
+
+		return true;
 	}
-	XMFLOAT3 DirectionalLight::Diffuse(XMFLOAT3 normal)
+	bool LightHandler::GeneratePoints(std::vector<PointLight>& lights)
 	{
-		// dot product?
-		return XMFLOAT3();
+		D3D11_BUFFER_DESC pointBufferDesc{};
+
+		pointBufferDesc.ByteWidth = sizeof(PointLight) * MAX_POINT_LIGHT;
+		pointBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+		pointBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		pointBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+
+		D3D11_SUBRESOURCE_DATA subData{};
+
+		subData.pSysMem = lights.data();
+
+		HRESULT result = mDevice->CreateBuffer(&pointBufferDesc, &subData, mPointLightBuffer.GetAddressOf());
+		if (result != S_OK)
+		{
+			Debug::Log("failed to generate real-time light buffer.");
+			return false;
+		}
+
+
 	}
-	void DirectionalLight::update(float delta)
-	{
-		Light::update(delta);
-	
-		XMStoreFloat3(&mDirection, mTransform->GetRotation());
-	}
+
+	//XMFLOAT3 PointLight::Diffuse(XMFLOAT3 normal)
+	//{
+	//	
+	//	return XMFLOAT3();
+	//}
+	//XMFLOAT3 DirectionalLight::Diffuse(XMFLOAT3 normal)
+	//{
+	//	float ndotl = dot(normal, XMFLOAT3(1, 1, 1));
+	//	
+	//	if (ndotl > 0.0f)
+	//	{
+	//		return XMFLOAT3(1, 0, 0);
+	//	}
+	//	else
+	//	{
+	//		return XMFLOAT3(1, 1, 0);
+	//	}
+	//	
+	//	return XMFLOAT3(1, 0, 1);
+	//}
+	//void DirectionalLight::update(float delta)
+	//{
+	//	Light::update(delta);
+	//
+	//	XMStoreFloat3(&mDirection, mTransform->GetRotation());
+	//}
 };
