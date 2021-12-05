858168a01cc20986ca4ef305f9e4d75b0786cd90
Ken <odessa5d4@gmail.com>
2021-12-05 19:38:28 +0900

    Light class rewrite

--- Narco/inc/Light.h
+++ Narco/inc/Light.h
@@ -1,131 +1,95 @@
 #pragma once
 #include<Common.h>
+
+#include"Transform.h"
 #include"Component.h"
 
 namespace NARCO
 {
-	class Transform;
-	class GameObject;
-	
-	enum eLightType
-	{
-		LIGHT_POINT,
-		LIGHT_DIRECTIONAL,
-	};
-
 	enum eLightMode
 	{
 		LIGHT_REALTIME,
 		LIGHT_BAKE,
 	};
 
+	enum eLightType
+	{
+		LIGHT_DIRECTIONAL,
+		LIGHT_POINT,
+	};
 
 
 	class Light : public Component
 	{
 	public:
-		struct Point
+		struct DirectionalLight
 		{
-			DirectX::XMFLOAT3 Position;
-			float Intesnity;
-		};
-
-		struct Directional
-		{
-			DirectX::XMFLOAT3 Direction;
+			XMFLOAT3 Direction;
 			float Intensity;
+			XMFLOAT4 Color;
+
+			bool operator==(const DirectionalLight& other)
+			{
+				XMVECTOR dir = XMLoadFloat3(&Direction), otherDir = XMLoadFloat3(&other.Direction);
+				XMVECTOR color = XMLoadFloat4(&Color), otherColor = XMLoadFloat4(&other.Color);
+
+				return XMVector3Equal(dir, otherDir) && XMVector3Equal(color, otherColor) && Intensity == other.Intensity;
+			}
 		};
 
-		Light(eLightType type, eLightMode mode);
-		~Light();
-	
-		eLightMode GetMode() const { return mMode; }
-		eLightType GetType() const { return mType; }
-	protected:
-		Transform* mTransform;
+		struct PointLight
+		{
+			XMFLOAT3 Position;
+			float Intensity;
+			XMFLOAT4 Color;
 
-		ComPtr<ID3D11ClassLinkage> mLinkage;
+			bool operator==(const PointLight& other)
+			{
+				XMVECTOR pos = XMLoadFloat3(&Position), otherPos = XMLoadFloat3(&other.Position);
+				XMVECTOR color = XMLoadFloat4(&Color), otherColor = XMLoadFloat4(&other.Color);
+
+				return XMVector3Equal(pos, otherPos) && XMVector3Equal(color, otherColor) && Intensity == other.Intensity;
+			}
+		};
+
+		eLightType GetType() const { return mType; }
+		eLightMode GetMode() const { return mMode; }
+
+		const DirectionalLight& GetDirectional() const { return mDirectional; }
+		const PointLight& GetPoint() const { return mPoint; }
+
+	private:
+		bool compare();
+		void notify();
+
+		static uint LightID;
+		const Transform* mTransform;
 
 		eLightType mType;
 		eLightMode mMode;
 
-		uint mSize;
+		DirectionalLight mDirectional;
+		DirectionalLight mPrevDirectional;
 
-	};
+		PointLight mPoint;
+		PointLight mPrevPoint;
 
-	class DirectionalLight : public Light
-	{
-	public:
-		struct Raw
-		{
-			XMFLOAT3 Direction;
-			float Intensity;
-
-			XMFLOAT4 Color;
-		};
-
-		DirectionalLight()
-			: Light(LIGHT_DIRECTIONAL, LIGHT_REALTIME)
-		{
-			XMVECTOR direction = XMVectorSet(500, 500, -500, 1.0f) - XMVectorSet(0, 0, 0, 1.0f);
-
-			Raw.Intensity = 1.0f;
-
-			XMStoreFloat3(&Raw.Direction, direction);
-			XMStoreFloat4(&Raw.Color, Colors::White);
-		}
-
-		DirectionalLight(const DirectionalLight& other)
-			: Light(other.mType, other.mMode), Raw(other.Raw)
-		{
-
-		}
-
-		virtual void awake() override;
-		virtual void start() override;
-		virtual void update(float delta) override;
-		virtual void render(float delta) override;
-		virtual void release() override;
-
-		~DirectionalLight() {}
+		float mIntensity;
+		XMFLOAT4 mColor;
 		
-		Raw Raw;
-	};
 
-	class PointLight : public Light
-	{
-	public:
-		struct Raw
-		{
-			XMFLOAT3 Position;
-			float Intensity;
+		uint mID;
 
-			XMFLOAT4 Color;
-		};
-
-		PointLight()
-			: Light(LIGHT_POINT, LIGHT_REALTIME)
-		{
-			XMVECTOR position = XMVectorSet(0, 0, 0, 1.0f);
-			
-			XMStoreFloat3(&Raw.Position, position);
-			XMStoreFloat4(&Raw.Color, Colors::White);
-		}
-		
-		PointLight(const PointLight& other)
-			: Light(other.mType, other.mMode), Raw(other.Raw)
-		{
-
-		}
-		~PointLight() {}
-
+		// Component을(를) 통해 상속됨
 		virtual void awake() override;
-		virtual void start() override;
-		virtual void update(float delta) override;
-		virtual void render(float delta) override;
-		virtual void release() override;
 
-		Raw Raw;
+		virtual void start() override;
+
+		virtual void update(float delta) override;
+
+		virtual void render(float delta) override;
+
+		virtual void release() override;
 
 	};
 }
